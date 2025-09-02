2775985a28695c28fa10f002f017f18c3bcc5ade
Blezant <blezdev97@gmail.com>
2025-09-02 01:19:57 -0700

    Set up of the stack navigation

---
+++ screens/LoginScreen.js
@@ -0,0 +1,17 @@
+import { SafeAreaView } from "react-native-safe-area-context";
+
+
+export default function LoginScreen(navigation){
+
+
+    return (
+<SafeAreaView>
+
+<Text>
+    Hi
+</Text>
+    
+</SafeAreaView>
+
+    )
+}
---
+++ screens/SignupScreen.js
@@ -0,0 +1,15 @@
+import { SafeAreaView } from "react-native-safe-area-context";
+
+
+export default function SignupScreen(navigation){
+
+
+    return (
+<SafeAreaView>
+
+
+    
+</SafeAreaView>
+
+    )
+}
